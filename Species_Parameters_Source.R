a4aec0a66ffe6382a49d8156140b55f832befa5f
Janette Davidson <jdavid21@uwyo.edu>
2022-02-02 21:55:53 -0700

    Updated invader sigmaD, added definitions of variables

--- Species_Parameters_Source.R
+++ Species_Parameters_Source.R
@@ -3,13 +3,21 @@
 # of the two resident and two invader species.
 #####################################
 
+#######Parameter Abbreviations#######
+#r - intrinsic growth rate
+#K - carrying capacity
+#sigmaD - demographic variance of species 
+#sigmaE - Environmental effect on species 
+#beta - competitive effect between species
+#####################################
+
 #--Resident Community Parameters-----
-env_condition <- seq(from=0,to=.25,by=.01)
-beta_range <- seq(from=0,to=.95, by=.05)
+env_condition <- seq(from=0, to=.25, by=.01)
+beta_range <- seq(from=0, to=.95, by=.05)
 time <- 300
 
-r1 <- 0.5       
-r2 <- 0.8       
+r1 <- 0.5
+r2 <- 0.8
 K1 <- 1000      
 K2 <- 1500      
 sigmaD1 <- 1    
@@ -23,7 +31,7 @@ beta32 <- 0.6
 beta13 <- 0.4  
 beta23 <- 0.4  
 sigmaE3 <- -0.06 
-sigmaD3 <- 0  
+sigmaD3 <- 1  
 
 #--Strong Invader Parameters-----
 r4 <- 0.7    
@@ -33,7 +41,7 @@ beta42 <- 0.5
 beta14 <- 0.5  
 beta24 <- 0.5 
 sigmaE4 <- -0.1
-sigmaD4 <- 0    
+sigmaD4 <- 1    
 
 
 
